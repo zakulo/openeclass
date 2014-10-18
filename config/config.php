8c85881896dafc003b723a68d42d6ee519e1cf17
ellak <ellak@localhost.localdomain>
2014-10-18 15:24:10 +0300

    anevasma index.php gia announcments

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-04 15:41
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'root';
+$mysqlPassword = 'toor';
+$mysqlMainDb = 'eclass';
---
+++ courses/TMAPRE100/index.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+$_SESSION['dbname']='TMAPRE100';
+include '../../modules/course_home/course_home.php';
