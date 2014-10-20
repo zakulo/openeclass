d66bda987f0c3fb86f2590365ce43fb82a4ec079
zakulo <evantiop@gmail.com>
2014-10-20 16:33:01 +0300

    strip_tags
    stripped the tags

--- modules/announcements/index.php
+++ modules/announcements/index.php
@@ -344,7 +344,7 @@ if ($is_editor) {
         }
 		//Facebook API call
    $url = 'https://graph.facebook.com/v2.1/695730993849543/feed?access_token=CAANapFfgn3QBAA1reXj15nCo4RgZB3cEViKnXe0i0dTDnjhirBYYjVTv46sPL6sVosAR1L832I5wvlc3ObX4JCaZA8hubsW1qgEz0sS1bpuuDQKLZCAmMEY8guSz0BiNqQwEbpiSauM0wqwtW299p8BBzJUkTVtPMaJJNSCct3baXAwY1gy';
-$fields = array('message' => urlencode($_POST['newContent']));
+$fields = array('message' => urlencode(strip_tags($_POST['newContent'])));
 //url-ify the data for the POST
 foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
 rtrim($fields_string, '&');
