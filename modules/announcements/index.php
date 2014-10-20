6d0e92dee81a6bc43af96ca872497b2a98bc78cb
zakulo <evantiop@gmail.com>
2014-10-20 16:14:18 +0300

    Facebook API call

--- modules/announcements/index.php
+++ modules/announcements/index.php
@@ -342,7 +342,23 @@ if ($is_editor) {
         else {
             $message = "<p class='success'>$langAnnAdd</p>";
         }
-    } // end of if $submit
+		//Facebook API call
+   $url = 'https://graph.facebook.com/v2.1/695730993849543/feed?access_token=CAANapFfgn3QBAA1reXj15nCo4RgZB3cEViKnXe0i0dTDnjhirBYYjVTv46sPL6sVosAR1L832I5wvlc3ObX4JCaZA8hubsW1qgEz0sS1bpuuDQKLZCAmMEY8guSz0BiNqQwEbpiSauM0wqwtW299p8BBzJUkTVtPMaJJNSCct3baXAwY1gy';
+$fields = array('message' => urlencode($_POST['newContent']));
+//url-ify the data for the POST
+foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+rtrim($fields_string, '&');
+//open connection
+$ch = curl_init();
+//set the url, number of POST vars, POST data
+curl_setopt($ch,CURLOPT_URL, $url);
+curl_setopt($ch,CURLOPT_POST, count($fields));
+curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+//execute post
+$result = curl_exec($ch);
+//close connection
+curl_close($ch); } // end of if $submit
 
 
     // teacher display
